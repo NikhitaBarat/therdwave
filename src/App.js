c21b27d8572df5bc3234f9f6e649c26b0d5cf1c4
Nikhita <bnikhita2003@gmail.com>
2022-04-14 18:53:42 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from './components/header/header.components'
+import Home from './routes/home/home.routes'
+import Dashboard from './routes/dashboard/dashboard.routes'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Home />
+      <Dashboard />
     </div>
   );
 }
