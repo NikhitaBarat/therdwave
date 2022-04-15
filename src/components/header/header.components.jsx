6ac87b14ad1df74ab96e34464e97229a26a2fdff
siddhantprateek <siddhantprateek@gmail.com>
2022-04-15 09:55:39 +0530

    🚩Header view fixed

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
-import './header.styles.css'
-import logo from '../../assets/logo.png'
+import React from "react"
+import "./header.styles.css"
+import logo from "../../assets/logo.png"
+import { Link } from 'react-router-dom'
+
+// MUI MODULES
+import SearchIcon from "@mui/icons-material/Search"
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined"
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined"
+
 
 function Header() {
   return (
     <header>
-    <div className='title'>
-      <img src={logo} alt='logo'/>
-    </div>
-    <div className='rest'>
-      Dashboard 
-        <SearchIcon fontSize='large' className='header_icon'/>
-        <AccountBalanceWalletOutlinedIcon fontSize='large' className='header_icon'/>
-        <PermIdentityOutlinedIcon fontSize='large' className='header_icon'/>
-    </div>
+      <div className="logo-container">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className="navigation-option">
+        <ul>
+          <li>
+            <Link to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <SearchIcon fontSize="medium" className="header_icon" />
+          </li>
+          <li>
+            <AccountBalanceWalletOutlinedIcon
+              fontSize="medium"
+              className="header_icon"
+            />
+          </li>
+          <li>
+            <PermIdentityOutlinedIcon
+              fontSize="medium"
+              className="header_icon"
+            />
+          </li>
+        </ul>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
-
+export default Header;
