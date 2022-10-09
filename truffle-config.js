b6ae121b25c99fdb80a800740d040145e117f2e9
Siddhant Prateek <siddhantprateek@gmail.com>
2022-10-10 01:54:12 +0530

    added Polygon configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
@@ -5,7 +9,19 @@ module.exports = {
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    // polygon network configuration
+    // matic: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+    //   network_id: 80001,
+    //   confirmations: 2,
+    //   timeoutBlocks: 200,
+    //   skipDryRun: true
+    // },
   },
+  
+  // mocha: {
+  //    timeout: 100000
+  // },
 
   // Configure your compilers
   compilers: {
