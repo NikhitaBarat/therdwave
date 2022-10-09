a36fdc50db07d67d2be1e5437fcea75515eda42e
Siddhant Prateek <siddhantprateek@gmail.com>
2022-10-09 17:20:16 +0530

    user artist Contract Need to be updated

--- contracts/userArtistContract.sol
+++ contracts/userArtistContract.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.15;
+import './ERC721Connector.sol';
 
-contract userArtistContract {
-  constructor() public {
+contract userArtistContract is ERC721Connector {
+
+  string public userName;
+  string public artistName;
+
+  constructor() ERC721Connector('UKN ARTIST', 'TRDUSD') {
+    artistName = "Unknown Artist";
+    userName = "Unknown User";
   }
 }
